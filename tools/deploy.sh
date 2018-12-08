84ca2971c9ff8b9844848bdc57ec0e4e4b8b55ce
Johan Guldmyr <martbhell@protonmail.com>
2018-12-08 21:26:36 +0200

    go run all except *_test.go files

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -8,7 +8,10 @@ fi
 
 echo "deploying to BUCKET: $BUCKET"
 gsutil stat gs://$BUCKET/feed.xml
-go run crawler.go -rss -topic GW,S3,keystone > feed.xml
+# https://stackoverflow.com/questions/28081486/how-can-i-go-run-a-project-with-multiple-files-in-the-main-package
+# go run all except *_test.go files (handy when we get around to add testing :)
+shopt -s extglob
+go run !(*_test).go -rss -topic GW,S3,keystone > feed.xml
 ls -l
 gsutil cp -a public-read feed.xml gs://$BUCKET/feed.xml
 
