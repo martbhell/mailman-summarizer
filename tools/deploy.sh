844bda1fdfeebf1de9a2d3c109220d03e8fc8062
Johan Guldmyr <martbhell@protonmail.com>
2018-12-17 20:57:49 +0200

    With bash compare the old and new feed.xml

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -15,9 +15,13 @@ wget https://storage.googleapis.com/$BUCKET/feed.xml -O /tmp/feed2.xml
 # https://stackoverflow.com/questions/28081486/how-can-i-go-run-a-project-with-multiple-files-in-the-main-package
 # go run all except *_test.go files (handy when we get around to add testing :)
 shopt -s extglob
-go run !(*_test).go -rss -topic GW,S3,keystone,civet > feed.xml
+go run !(*_test).go -rss -topic GW,gw,S3,s3,keystone,civet > feed.xml
 echo "TEST: Showing diff between old and new feed.xml"
 diff -u /tmp/feed2.xml feed.xml
+# diff existing and now built feed.xml. Only show lines that have changed. Exclude some patterns and count the rest.
+diff -u /tmp/feed2.xml feed.xml|grep "^+\ "|grep -c -v -e pubDate -e link -e guid
+NUMCHANGEDLINES=$(diff -u /tmp/feed2.xml feed.xml|grep "^+\ "|grep -c -v -e pubDate -e link -e guid)
+echo "TEST: number of changed content: $NUMCHANGEDLINES"
 echo "TEST: listing dir"
 ls -l
 echo "DEPLOY: uploading feed.xml publicly to $BUCKET"
