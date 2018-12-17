0b79c0ebc31e7422016e0a775b68aebaeb735471
Johan Guldmyr <johan.guldmyr@csc.fi>
2018-12-17 09:49:56 +0200

    Test/deploy: diff feed.xml and also match civet
     - Diffing to get an idea what changes between runs
     - Include civetweb because that's interesting
     - Add some "tags" like TEST: and DEPLOY: that one can search for in the
       log output

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -6,12 +6,20 @@ if [ x"$BUCKET" == "x" ]; then
 	exit 1
 fi
 
-echo "deploying to BUCKET: $BUCKET"
+echo "TEST: deploying to BUCKET: $BUCKET"
 gsutil stat gs://$BUCKET/feed.xml
+
+echo "TEST: fetching feed.xml before compiling and putting it in /tmp/feed2.xml"
+wget https://storage.googleapis.com/ceph-rgw-users/feed.xml -O /tmp/feed2.xml
+
 # https://stackoverflow.com/questions/28081486/how-can-i-go-run-a-project-with-multiple-files-in-the-main-package
 # go run all except *_test.go files (handy when we get around to add testing :)
 shopt -s extglob
-go run !(*_test).go -rss -topic GW,S3,keystone > feed.xml
+go run !(*_test).go -rss -topic GW,S3,keystone,civet > feed.xml
+echo "TEST: Showing diff between old and new feed.xml"
+diff -u /tmp/feed2.xml feed.xml
+echo "TEST: listing dir"
 ls -l
+echo "DEPLOY: uploading feed.xml publicly to $BUCKET"
 gsutil cp -a public-read feed.xml gs://$BUCKET/feed.xml
 
