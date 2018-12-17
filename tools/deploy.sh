9f66692d900d0b6f2f606c4244033eb2cdc0dbae
Johan Guldmyr <martbhell@protonmail.com>
2018-12-17 21:07:32 +0200

    OK! Now let's only deploy if stuff has changed

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -19,11 +19,14 @@ go run !(*_test).go -rss -topic GW,gw,S3,s3,keystone,civet > feed.xml
 echo "TEST: Showing diff between old and new feed.xml"
 diff -u /tmp/feed2.xml feed.xml
 # diff existing and now built feed.xml. Only show lines that have changed. Exclude some patterns and count the rest.
-diff -u /tmp/feed2.xml feed.xml|grep "^+\ "|grep -c -v -e pubDate -e link -e guid
 NUMCHANGEDLINES=$(diff -u /tmp/feed2.xml feed.xml|grep "^+\ "|grep -c -v -e pubDate -e link -e guid)
 echo "TEST: number of changed content: $NUMCHANGEDLINES"
 echo "TEST: listing dir"
 ls -l
-echo "DEPLOY: uploading feed.xml publicly to $BUCKET"
-gsutil cp -a public-read feed.xml gs://$BUCKET/feed.xml
+if [ $NUMCHANGEDLINES -gt 0 ]; then
+	echo "DEPLOY: uploading feed.xml publicly to $BUCKET"
+	gsutil cp -a public-read feed.xml gs://$BUCKET/feed.xml
+else
+	echo "NOT DEPLOYING: $NUMCHANGEDLINES in the feed.xml"
+fi
 
